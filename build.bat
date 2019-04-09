7fa9f2e80d007b9d4bd0b9b5748d3a7a48a79e78
Sophia <async3619@naver.com>
2019-04-09 13:53:03 +0900

    update build script

--- build.bat
+++ build.bat
@@ -6,65 +6,27 @@ git clone --depth 1 -b master https://github.com/boostorg/boost
 cd ./boost
 
 git submodule init tools/build tools/boostdep tools/boost_install libs/headers libs/config libs/filesystem
-git submodule update --jobs %GIT_FETCH_JOBS% --depth 1
+git submodule update --jobs %GIT_FETCH_JOBS%
 
-python tools/boostdep/depinst/depinst.py --git_args "--jobs %GIT_FETCH_JOBS% --depth 1" filesystem
+python tools/boostdep/depinst/depinst.py --git_args "--jobs %GIT_FETCH_JOBS%" filesystem
 
 cmd /c .\bootstrap.bat
 
+set "BUILD_OPTION= toolset=msvc-14.0 link=static runtime-link=static threading=multi warnings=off"
+set "BUILD_OPTION= %BUILD_OPTION% define=BOOST_TYPE_INDEX_FORCE_NO_RTTI_COMPATIBILITY"
+
 IF "%CONFIGURATION%"=="Debug" (
-    set "BOOST_VARIANT=debug"
+    set "BUILD_OPTION= %BUILD_OPTION% variant=debug debug-symbols=on optimization=off"
 ) ELSE (
-    set "BOOST_VARIANT=release"
+    set "BUILD_OPTION= %BUILD_OPTION% variant=release debug-symbols=off optimization=speed"
 )
 
 REM build first
-.\b2.exe^
- -a^
- toolset=msvc-14.0^
- address-model=32,64^
- link=static^
- runtime-link=static^
- variant=%BOOST_VARIANT%^
- debug-symbols=off^
- optimization=speed^
- threading=multi^
- warnings=off^
- define=BOOST_TYPE_INDEX_FORCE_NO_RTTI_COMPATIBILITY^
- stage
+.\b2.exe -a %BUILD_OPTION% address-model=32,64 stage
 
 REM then install it into separated path
-.\b2.exe^
- -d0^
- -j4^
- --prefix=./%BRANCH_NAME%-x86^
- toolset=msvc-14.0^
- address-model=32^
- link=static^
- runtime-link=static^
- variant=release^
- debug-symbols=off^
- optimization=speed^
- threading=multi^
- warnings=off^
- define=BOOST_TYPE_INDEX_FORCE_NO_RTTI_COMPATIBILITY^
- install
-
-.\b2.exe^
- -d0^
- -j4^
- --prefix=./%BRANCH_NAME%-x64^
- toolset=msvc-14.0^
- address-model=64^
- link=static^
- runtime-link=static^
- variant=release^
- debug-symbols=off^
- optimization=speed^
- threading=multi^
- warnings=off^
- define=BOOST_TYPE_INDEX_FORCE_NO_RTTI_COMPATIBILITY^
- install
+.\b2.exe -d0 -j4 --prefix=./%BRANCH_NAME%-x86 address-model=32 %BUILD_OPTION% install
+.\b2.exe -d0 -j4 --prefix=./%BRANCH_NAME%-x64 address-model=64 %BUILD_OPTION% install
 
 REM get boost versioning name to interpolate path
 powershell -c "Set-Content .boost_version (Get-ChildItem .\%BRANCH_NAME%-x86\include\)[0].Name"
--- build.sh
+++ build.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
 export GIT_FETCH_JOBS=8
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    export GIT_FETCH_DEPTH="--depth 1";
-fi
-
 
 git clone --depth 1 -b master https://github.com/boostorg/boost
 cd ./boost
 
 git submodule init tools/build tools/boostdep tools/boost_install libs/headers libs/config libs/filesystem
-git submodule update --jobs ${GIT_FETCH_JOBS} ${GIT_FETCH_DEPTH}
+git submodule update --jobs ${GIT_FETCH_JOBS}
 
-python tools/boostdep/depinst/depinst.py --git_args "--jobs ${GIT_FETCH_JOBS} ${GIT_FETCH_DEPTH}" filesystem
+python tools/boostdep/depinst/depinst.py --git_args "--jobs ${GIT_FETCH_JOBS}" filesystem
 
 bash ./bootstrap.sh
 
-# build first
-./b2 -j${CPU_COUNTS} address-model=32 link=static variant=${BOOST_VARIANT} debug-symbols=off optimization=speed threading=multi warnings=off define=BOOST_TYPE_INDEX_FORCE_NO_RTTI_COMPATIBILITY stage
-./b2 -d0 -j${CPU_COUNTS} --prefix=./${BRANCH_NAME}-x86 address-model=32 link=static variant=${BOOST_VARIANT} debug-symbols=off optimization=speed threading=multi warnings=off define=BOOST_TYPE_INDEX_FORCE_NO_RTTI_COMPATIBILITY install
+export BUILD_OPTION="link=static threading=multi warnings=off define=BOOST_TYPE_INDEX_FORCE_NO_RTTI_COMPATIBILITY"
 
-./b2 -j${CPU_COUNTS} address-model=64 link=static variant=${BOOST_VARIANT} debug-symbols=off optimization=speed threading=multi warnings=off define=BOOST_TYPE_INDEX_FORCE_NO_RTTI_COMPATIBILITY stage
-./b2 -d0 -j${CPU_COUNTS} --prefix=./${BRANCH_NAME}-x64 address-model=64 link=static variant=${BOOST_VARIANT} debug-symbols=off optimization=speed threading=multi warnings=off define=BOOST_TYPE_INDEX_FORCE_NO_RTTI_COMPATIBILITY install
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+    export BUILD_OPTION="${BUILD_OPTION} variant=debug debug-symbols=on optimization=off";
+else
+    export BUILD_OPTION="${BUILD_OPTION} variant=release debug-symbols=off optimization=speed";
+fi
+
+# build first
+./b2 -j${CPU_COUNTS} address-model=32 ${BUILD_OPTION} stage
+./b2 -d0 -j${CPU_COUNTS} --prefix=./${BRANCH_NAME}-x86 address-model=32 ${BUILD_OPTION} install
+
+./b2 -j${CPU_COUNTS} address-model=64 ${BUILD_OPTION} stage
+./b2 -d0 -j${CPU_COUNTS} --prefix=./${BRANCH_NAME}-x64 address-model=64 ${BUILD_OPTION} install
 
 cd ..
